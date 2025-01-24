dc1e3d9ede3623a5bec0a43d5262bf8be5335c69
vwency <duckdodo666@gmail.com>
2025-01-25 00:49:47 +0600

    chore: vite prod conf

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,36 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
+export default defineConfig(() => {
+  const isProd = process.env.VITE_APP_ENV === 'production';
 
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    sourcemap: true,
-  },
+  return {
+    root: path.resolve(__dirname, 'src'),
 
-  plugins: [react()],
-
-  server: {
-    port: 9000,
-    open: true,
-    historyApiFallback: true,
-  },
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    build: {
+      outDir: path.resolve(__dirname, 'dist'),
+      sourcemap: !isProd, 
+      terserOptions: isProd
+        ? {
+            compress: {
+              drop_console: true, 
+            },
+          }
+        : {},
     },
-  },
+
+    plugins: [react()],
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+
+    server: {
+      port: 9000,
+      open: true,
+      historyApiFallback: true,
+    },
+  };
 });
