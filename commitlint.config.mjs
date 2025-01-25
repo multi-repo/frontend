172ad3135243eb84976b745182696c77df965850
vwency <duckdodo666@gmail.com>
2025-01-25 16:23:27 +0600

    chore: file extensions

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,8 @@
-module.exports = {
+const config = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 72],
   },
 };
+
+export default config;
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // src/App.jsx
-import React from 'react';
-import './styles/main.scss'; // Импортируем SCSS файл
-import AuthLayout from './Components/AuthForm/AuthMain';
-import MainLayout from './Components/Main';
+import React from 'react'
+import './styles/main.scss' // Импортируем SCSS файл
+import AuthLayout from './Components/AuthForm/AuthMain'
+import MainLayout from './Components/Main'
 
 const App = () => {
-  return (
-    <div className="app">
-      <MainLayout />
-    </div>
-  );
-};
+    return (
+        <div className="app">
+            <MainLayout />
+        </div>
+    )
+}
 
-export default App;
+export default App
