257fdb2b06be093dc205e0a32dfa9a3f7cb338d9
vwency <duckdodo666@gmail.com>
2025-04-16 01:30:50 +0600

    chore: duplicate home layout removed

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -7,7 +7,6 @@ const Slider = React.lazy(() => import('@/Components/MainSlider/Slider.jsx'))
 const HomeLayout = () => (
   <Suspense fallback={<div>Загрузка...</div>}>
     <MenuMainLayout />
-    <MenuMainLayout />
     <Slider />
     <Outlet />
   </Suspense>
